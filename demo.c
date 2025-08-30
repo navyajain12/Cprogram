0c0bbe08d21b58dc3f21aade930f5a88e65ada9a
navyajain12 <navyajain12002@gmail.com>
2025-08-30 16:36:31 +0530

    first commit: added demo.c

---
+++ demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Input 2 numbers\n");
+    scanf("%d%d",&a,&b);
+    sum = a+b;
+    printf("Sum of the 2 numbers: %d",sum);
+    return 0;
+}
